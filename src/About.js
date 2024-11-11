e61fe301399a41216fad57f962bb34fd73415455
ahmadshofi23 <ahmadshofihasibuan12@gmail.com>
2024-11-12 01:09:57 +0700

    first project

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+      <h1>About Us</h1>
+      <p>This is the About page of our React application.</p>
+    </div>
+  );
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div style={{ textAlign: 'center' }}>
+        <nav style={{ margin: '20px' }}>
+          <Link to="/" style={{ margin: '10px' }}>Home</Link>
+          <Link to="/about" style={{ margin: '10px' }}>About</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
